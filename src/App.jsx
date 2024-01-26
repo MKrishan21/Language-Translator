17252a36dac591014f4afa1445d9e2e839211536
MKrishan21 <kgmishra21@gmail.com>
2024-01-26 13:09:59 +0530

    'translator'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Translator from './components/Translator';
+
+function App(){
+    return(
+        <div classNamme="App">
+         <div class="title">CODECLAUSE TRANSLATOR</div>
+            <Translator />
+        </div>
+    );
+}
+
+export default App;
